b81ea2721d28852b1b772008eefef428dd99d236
Kabiir <kabiirk@gmail.com>
2021-06-28 00:08:55 +0530

    ffmpeg demo working now

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
-const ffmpeg = createFFmpeg({ log: true });
+const ffmpeg = createFFmpeg({
+  corePath: 'https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js',
+   log: true });
 
 function App() {
   const [ready, setReady] = useState(false);
   const [video, setVideo] = useState();
   const [gif, setGif] = useState();
 
-  const load = async() => {
+  const load = async () => {
     await ffmpeg.load();
     setReady(true);
   }
@@ -19,39 +21,45 @@ function App() {
   }, [])
 
   const convertToGif = async () => {
-    // writing file to file storage
-    ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video) );
+    // Write the file to memory 
+    ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video));
 
-    await ffmpeg.run('-i', 'test.mp4', '-t', '2.5', '-ss', '-f', 'gif', 'out.gif');
+    // Run the FFMpeg command
+    await ffmpeg.run('-i', 'test.mp4', '-t', '2.5', '-ss', '2.0', '-f', 'gif', 'out.gif');
 
+    // Read the result
     const data = ffmpeg.FS('readFile', 'out.gif');
 
-    const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif'}));
-    setGif(url);
+    // Create a URL
+    const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
+    setGif(url)
   }
 
-  
   return ready ? (
+    
     <div className="App">
       { video && <video
-                controls
-                width="250"
-                src={URL.createObjectURL(video)}>
-        </video>}
+        controls
+        width="250"
+        src={URL.createObjectURL(video)}>
+
+      </video>}
+
 
       <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
 
       <h3>Result</h3>
+
       <button onClick={convertToGif}>Convert</button>
 
-      { gif && <img alt='' src={gif} width="250" /> }
-
+      { gif && <img src={gif} width="250" alt="out.gif"/>}
 
     </div>
-  ) :
-  (
-    <p>Loading....</p>
-  );
+  )
+    :
+    (
+      <p>Loading...</p>
+    );
 }
 
 export default App;
